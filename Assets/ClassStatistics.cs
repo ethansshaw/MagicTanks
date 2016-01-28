16d4363bfa9efcd3998ff1e99a67c8949e4e69fc
= <Ethan Shaw>
2016-01-28 13:12:37 -0500

    Added script for movement

---
+++ Assets/ClassStatistics.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassStatistics : MonoBehaviour {
+
+	public int attackDamage;
+	public int armour;
+	public float speed;
+
+
+	public void Viper(){
+		attackDamage = 3;
+		speed = 12f;
+		armour = 6;
+		Debug.Log ("Viper has now been chosen");
+	}
+
+	public void Panther(){
+		attackDamage = 7;
+		speed = 7;
+		armour = 7;
+		Debug.Log ("Panther has now been chosen");
+	}
+
+	public void Eagle(){
+		attackDamage = 3;
+		speed = 6;
+		armour = 12;
+		Debug.Log ("Eagle has now been chosen");
+	}
+
+	public void Rhino(){
+		attackDamage = 12;
+		speed = 3;
+		armour = 6;
+		Debug.Log ("Rhino has now been chosen");
+	}
+
+}
+
+
---
+++ Assets/MenuNavigation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour {
+
+	public void gotoTankSelection(){
+		SceneManager.LoadScene ("TankSelection");
+	}
+
+	public void gotoMainMenu(){
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void playGame(){
+		SceneManager.LoadScene("Game");
+	}
+}
